a90d006680dcf466a55ea542bd8c022cbfa39aa3
dmi3midd <dmi3midd@gmail.com>
2026-06-01 21:57:41 +0300

    feat: update EmailRepository.

--- internal/email/repository.go
+++ internal/email/repository.go
@@ -2,7 +2,6 @@ package email
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
 
@@ -14,9 +13,8 @@ var (
 )
 
 type EmailRepository interface {
-	GetByID(ctx context.Context, id string) (*Email, error)
-	GetAll(ctx context.Context) ([]Email, error)
 	Create(ctx context.Context, email *Email) error
+	CreateMany(ctx context.Context, emails []Email) error
 }
 
 type emailRepository struct {
@@ -29,38 +27,6 @@ func NewEmailRepository(db *sqlx.DB) EmailRepository {
 	}
 }
 
-func (r *emailRepository) GetByID(ctx context.Context, id string) (*Email, error) {
-	op := "EmailRepository.GetByID"
-	query := `
-	SELECT id, sender, reciever, subject, body, sent_at
-	FROM emails
-	WHERE id = $1
-	`
-	var email Email
-	err := r.DB.GetContext(ctx, &email, query, id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, fmt.Errorf("%s: %w", op, ErrNoEmail)
-		}
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-	return &email, nil
-}
-
-func (r *emailRepository) GetAll(ctx context.Context) ([]Email, error) {
-	op := "EmailRepository.GetAll"
-	query := `
-	SELECT id, sender, reciever, subject, body, sent_at
-	FROM emails
-	`
-	var emails []Email
-	err := r.DB.SelectContext(ctx, &emails, query)
-	if err != nil {
-		return []Email{}, fmt.Errorf("%s: %w", op, err)
-	}
-	return emails, nil
-}
-
 func (r *emailRepository) Create(ctx context.Context, email *Email) error {
 	op := "EmailRepository.Create"
 	query := `
@@ -68,6 +34,19 @@ func (r *emailRepository) Create(ctx context.Context, email *Email) error {
 	VALUES (:id, :sender, :reciever, :subject, :body, :sent_at)
 	`
 	_, err := r.DB.NamedExecContext(ctx, query, email)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (r *emailRepository) CreateMany(ctx context.Context, emails []Email) error {
+	op := "EmailRepository.CreateMany"
+	query := `
+	INSERT INTO emails (id, sender, reciever, subject, body, sent_at)
+	VALUES (:id, :sender, :reciever, :subject, :body, :sent_at)
+	`
+	_, err := r.DB.NamedExecContext(ctx, query, emails)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
